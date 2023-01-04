82da45a467aad3e84cfa2bf0b476acdbc39c6f0d
melisa108 <kagyulife@gmail.com>
2023-01-04 15:19:14 +0100

    added remote url

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -14,6 +14,8 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1500x600";
         Configuration.holdBrowserOpen = true;
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
